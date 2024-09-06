de4b0bf9229308a9f62cd7760b2a0f654ac26440
Chris Moyd <christopher.moyd@caci.com>
2024-09-06 18:07:52 -0400

    02-Multi-Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_tutorial/main_model.dart';
+import 'package:provider_tutorial/second_counter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +12,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return ChangeNotifierProvider(
+        create: (_) => MainModel(),
+        child: MaterialApp(
+          title: 'Flutter State Management Basics',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const MyHomePage(title: 'Flutter State Management Basics'),
+        ));
   }
 }
 
@@ -34,34 +37,44 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-        create: (_) => MainModel(),
-        child: Consumer<MainModel>(
-            builder: (context, model, child) => Scaffold(
-                  appBar: AppBar(
-                    backgroundColor:
-                        Theme.of(context).colorScheme.inversePrimary,
-                    title: Text(widget.title),
-                  ),
-                  body: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        const Text(
-                          'You have pushed the button this many times:',
-                        ),
-                        Text(
-                          '${model.counter}',
-                          style: Theme.of(context).textTheme.headlineMedium,
-                        ),
-                      ],
+    return Consumer<MainModel>(
+        builder: (context, model, child) => Scaffold(
+              appBar: AppBar(
+                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                title: Text(widget.title),
+              ),
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'You have pushed the button this many times:',
                     ),
-                  ),
-                  floatingActionButton: FloatingActionButton(
-                    onPressed: () => model.counter++,
-                    tooltip: 'Increment',
-                    child: const Icon(Icons.add),
-                  ),
-                )));
+                    Text(
+                      '${model.counter}',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: TextButton(
+                        child: const Text('Open Counter 2'),
+                        onPressed: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                const SecondCounter(title: 'Second Counter'),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () => model.counter++,
+                tooltip: 'Increment',
+                child: const Icon(Icons.add),
+              ),
+            ));
   }
 }
